7827c950da868b9d9ce4fd7b68673321788e0a90
Quang Ho <157239q@gmail.com>
2021-08-11 23:05:42 +0700

    Added k1lib.bioinfo package

---
+++ k1lib/bioinfo/cli/__init__.py
@@ -0,0 +1,20 @@
+from .init import settings as bioinfoSettings
+
+# common utils
+from .filt import *
+from .grep import *
+from .input import *
+from .modifier import *
+from .output import *
+from .structural import *
+from .utils import *
+
+# bio specific
+from .entrez import *
+from .bio import *
+from . import mgi
+
+# file formats
+from . import kxml
+from . import kcsv
+from . import sam
---
+++ k1lib/bioinfo/readme.md
@@ -0,0 +1,4 @@
+
+# k1lib.bioinfo
+
+This is a sub project, quite unrelated to the main ML library, for bioinformatics tools here and there
